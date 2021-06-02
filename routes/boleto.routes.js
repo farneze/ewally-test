e3f6f5688377cdcc883e66ac8889cfa95095d61a
Arthur <arthurfarneze@gmail.com>
2021-06-01 21:55:02 -0300

    Added barcode character and special character handling, and an error catch for it. Also, added error for wrong routes

--- routes/boleto.routes.js
+++ routes/boleto.routes.js
@@ -3,23 +3,33 @@ const router = require("express").Router();
 // Metodo get para a rota boleto
 router.get('/:barcode', (req, res) => {
     try { 
-
+        
         // Coleta o valor do parametro de rota 'barcode' do request
         const { barcode } = req.params;
-        
-        console.log(barcode);
-        // res.send('<h1>Barcode: ' + barcode + '</h1>');
-        
-        // Retorna status 'OK' com as chaves e valores do cod. de barras
-        return res.status(200).json({ 
-            // barCode: `${barcode}`
-            barCode: `${barcode}`,
-            amount: `${barcode}`,
-            expirationDate: `${barcode}` 
-        });
+
+        // Filtro em regex. Padrão para encontrar characteres de a-z e A-Z
+        // e alguns caracteres especiais #?!@$%^&*-
+        const regex = /[a-z]|[#?!@$%^&*-]/gi;
+
+        // Verifica se o barcode passa no filtro.
+        const letters = barcode.match(regex);
+
+        // Se não houver match na filtragem anterior, letters será null
+        if(letters == null){
+
+            return res.status(200).json({ 
+                // barCode: `${barcode}`
+                barCode: `${barcode}`,
+                amount: `${barcode}`,
+                expirationDate: `${barcode}` 
+            });
+        } else {
+            throw new Error('Código de barras não pode conter letras ou caracteres especiais');
+        }
+
 
     } catch (err) {
-        return res.status(500).json({ error: `${err}` });
+        return res.status(500).json({ error: `${err.message}` });
     }
 });
 
--- routes/home.routes.js
+++ routes/home.routes.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 
+router.get('/:any', (req, res) => {
+    res.send('<h1> Invalid route </h1>');
+});
+
 router.get('/', (req, res) => {
     res.send('<h1> Invalid route </h1>');
 });
